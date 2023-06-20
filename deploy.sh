524deceb53c744d0882cd49cbe228c018e9aca24
balajigovinda <balajik9396@gmail.com>
2023-06-20 08:19:23 +0000

    Created deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+this file it will be deployment purpose only no one use this file please understand the rules 
---
+++ deploy1.sh
@@ -0,0 +1,1 @@
+this the also one file called as git file please consider as a github files 
